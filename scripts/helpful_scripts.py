7258464446a378c913a1d98128d4c86c062a4695
johnretsas <joretsas@gmail.com>
2021-11-14 15:19:33 +0000

    My first ERC20 token

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,25 @@
+from brownie import (
+    accounts,
+    network,
+    config,
+)
+from brownie.project.main import new
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    # accounts[0]
+    # accounts.add("env")
+    # accounts.load("id")
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        print(accounts[0].balance)
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
